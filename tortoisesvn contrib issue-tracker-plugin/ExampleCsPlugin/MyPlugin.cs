6ab4b0b6747626bf52517e2cf51a3ceab1849c4c
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2011-11-07 21:14:10 -0800

    fix nmbers in issues window, case 10 installer, still needs work, but works, case 11

--- tortoisesvn contrib issue-tracker-plugin/ExampleCsPlugin/MyPlugin.cs
+++ tortoisesvn contrib issue-tracker-plugin/ExampleCsPlugin/MyPlugin.cs
@@ -99,29 +99,27 @@ namespace ExampleCsPlugin
                 //revPropValues[0] = "13, 16, 17";
                 //revPropValues[1] = "myownvalue";
 
-                string added = "";
-                for (n = 0; n < ILEs.Count; n++) 
-                {
-                    added += "," + ILEs[n].Number.ToString();
-                }
-
-                bugIDOut = bugID + added;
-
-                MyIssuesForm form = new MyIssuesForm( tickets );
-                if ( form.ShowDialog( ) != DialogResult.OK )
-                    return originalMessage;
-
                 StringBuilder result = new StringBuilder( originalMessage );
                 if ( originalMessage.Length != 0 && !originalMessage.EndsWith( "\n" ) )
                     result.AppendLine( );
 
-                foreach ( TicketItem ticket in form.TicketsFixed )
+                MyIssuesForm form = new MyIssuesForm(tickets);
+
+                string added = "";
+
+                foreach (TicketItem ticket in form.TicketsFixed)
                 {
                     result.AppendFormat( "Fixed #{0}: {1}", ticket.Number, ticket.Summary );
                     result.AppendLine( );
                     selectedTickets.Add( ticket );
+
+                    added += "," + ticket.Number.ToString();
                 }
 
+                bugIDOut = bugID + added;
+
+                if (form.ShowDialog() != DialogResult.OK)
+                    return originalMessage;
 
                 return result.ToString( );
             }
